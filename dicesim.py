b0cb484d77384250bf24bdd86887bf82a16400bf
Santhosh Nayak <48375730+iamnyk7@users.noreply.github.com>
2020-08-01 16:37:03 +0530

    Add files via upload

---
+++ dicesim.py
@@ -0,0 +1,19 @@
+import random
+list=[1,2,3,4,5,6,]
+num=random.choice(list)
+if num==1:
+    print("| * |")
+if num==2:
+    print("|* *|")
+if num==3:
+    print("|* * *|")
+if num==4:
+    print("|* *|")
+    print("|* *|")
+if num==5:
+    print("|* *|")
+    print("| * |")
+    print("|* *|")
+if num==6:
+    print("|* * *|")
+    print("|* * *|")
